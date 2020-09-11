17b64b43e7cd6026f1f66f34ddd9ce88cb8d8e70
Sree Charitha <38373549+RSreeCharitha@users.noreply.github.com>
2020-09-11 11:15:48 +0530

    Update en_vernam.py
    Added comments

--- sourcecode/en_vernam.py
+++ sourcecode/en_vernam.py
@@ -1,12 +1,16 @@
-#VERNAM ENCRYPTION
-
+#VIGNERE ENCRYPTION
 # Plain text and keyword length must be same
-# Ei = (Pi+Ki) mod 26
+# For generating key, the given keyword is repeated in a circular manner until it matches the length of the plain text.
+# Ei = (Pi + Ki) mod 26
 p = input('Enter plain text: ')
 k = input('Enter keyword: ')
 
 p = p.upper()
 k = k.upper()
 
-for i in range(len(p)):
-    print(chr(((ord(p[i])+ord(k[i])-65-65) %26)+65),end='')
+l=len(p)
+k*=(len(p)//len(k))+1
+k = k[:l]
+print('Cipher key: ',end='')
+for i in range(l):
+    print(chr(((ord(p[i])+ord(k[i])-130)%26)+65),end='')
